8c2d363b341ce7117b5463d37e2e782dddabe910
T Hindersinn <theresa.hindersinn@gmail.com>
2017-08-26 02:03:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // write your code below!
+
+function happyHolidays(){
+  return("Happy holidays!")
+}
+
+function happyHolidaysto(holiday, name){
+  return("Happy ${holiday}, ${name}!")
+}
+
+function holidayCountdown(days, holiday){
+  return ("It's ${days} days until ${holiday}!")
+}
+
